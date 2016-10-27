0ab679c5823e268b3ae8f8622cbc9c37f3f66cfe
USER <aa@aa.com>
2016-10-27 17:13:57 +0900

    modified

--- app/src/main/java/com/fcmsender2/FcmSender.java
+++ app/src/main/java/com/fcmsender2/FcmSender.java
@@ -13,31 +13,37 @@ import java.net.URL;
  * Created by USER on 2016-10-25.
  */
 public class FcmSender {
-    public static final String SERVER_FCM = "https://fcm.googleapis.com/fcm/send";
+    public static final String FCM_SERVER_URL = "https://fcm.googleapis.com/fcm/send";
+    public static String apiKey = null;
 
+    public FcmSender(String apiKey) {
+        this.apiKey = apiKey;
+    }
 
-    public void main() {
+    /**
+     * @param tokenOrTopic topicsType : /topics/topicName ex) /topics/news
+     * @param msg
+     * @return
+     */
+    public static void sendMsg(String apiKey, String tokenOrTopic, String msg) {
         try {
-            URL url = new URL(SERVER_FCM);
+            URL url = new URL(FCM_SERVER_URL);
             HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
-            httpConn.setRequestProperty("Authorization", "key=" + "AIzaSyAN7qcnJgV5tlNx35-2J4xq-krVLIo2CQw");
+            httpConn.setRequestProperty("Authorization", "key=" + apiKey);
             httpConn.setRequestProperty("Content-Type", "application/json");
             httpConn.setRequestMethod("POST");
             httpConn.setDoOutput(true);
-
-            // Send GCM message content.
+            //
             OutputStream outputStream = httpConn.getOutputStream();
             // String token = "duOvNQ2bQzA:APA91bGNkagnaDMNwZnUx1T_FcI6sdnp42yAS4GDnWIrcRqzYe-0D92MszxDq5NDeUtJgTz5z45o6T6uEBuByRgZ4bBFEiJZVVlyilUp9dJeVeE86_NuQwFqEXzgR9PWsKfYr0Pv9L-s";
             // String token = "cI9PzZupjCI:APA91bEHBnG58i6yNO_D-Jgr5UXxFCfH5GlMv7olr-EDx0f0chTuwDk0s1Tcjh0yJOQMqndeC7Gmx6UagS3eJ9zoFRZMiiDoVK6lpGXW5WD8_PIxzqmEECfN4bJbMr_O9LrUb1-mfdCM";
-            String token = "/topics/news";
-            outputStream.write(getPayload(token, "newsTopic"));
+            // String token = "/topics/news";
+            outputStream.write(getPayload(tokenOrTopic, msg));
             outputStream.flush();
-
+            //
             InputStream inputStream = httpConn.getInputStream();
             String resp = inputStream.toString();
-            System.out.println(resp);
-            System.out.println("Check your device/emulator for notification or logcat for " +
-                    "confirmation of the receipt of the GCM message.");
+            System.out.println("resp : " + resp);
         } catch (IOException e) {
             System.out.println("Unable to send GCM message.");
             System.out.println("Please ensure that API_KEY has been replaced by the server " +
@@ -46,14 +52,7 @@ public class FcmSender {
         }
     }
 
-    /**
-     *
-     * @param tokenOrTopics
-     * topicsType : /topics/topicName ex) /topics/news
-     * @param msg
-     * @return
-     */
-    public byte[] getPayload(String tokenOrTopics, String msg) {
+    private static byte[] getPayload(String tokenOrTopics, String msg) {
         JSONObject jGcmData = new JSONObject();
         try {
             jGcmData.put("to", tokenOrTopics);
@@ -66,11 +65,11 @@ public class FcmSender {
         return jGcmData.toString().getBytes();
     }
 
-    public JSONObject getJSONData(String msg) {
+    private static JSONObject getJSONData(String msg) {
         JSONObject jData = new JSONObject();
         try {
-            jData.put("message", "mm" + msg);
-            jData.put("body", "bb" + msg);
+            jData.put("message", "message");
+            jData.put("body", msg);
             jData.put("title", "Notification_title");
             jData.put("icon", "icon");
         } catch (JSONException e) {
@@ -78,10 +77,5 @@ public class FcmSender {
         }
         return jData;
     }
-
-    private static boolean isEmpty(String s) {
-        return (s == null || "".equals(s));
-    }
-
 }
 
--- app/src/main/java/com/fcmsender2/MainActivity.java
+++ app/src/main/java/com/fcmsender2/MainActivity.java
@@ -12,7 +12,9 @@ public class MainActivity extends AppCompatActivity {
         new AsyncTask<String, String, String>(){
             @Override
             protected String doInBackground(String... params) {
-                new FcmSender().main();
+                String apiKey = "AIzaSyAN7qcnJgV5tlNx35-2J4xq-krVLIo2CQw";
+                String token = "duOvNQ2bQzA:APA91bGNkagnaDMNwZnUx1T_FcI6sdnp42yAS4GDnWIrcRqzYe-0D92MszxDq5NDeUtJgTz5z45o6T6uEBuByRgZ4bBFEiJZVVlyilUp9dJeVeE86_NuQwFqEXzgR9PWsKfYr0Pv9L-s";
+                FcmSender.sendMsg(apiKey, token, "sendTest");
                 return null;
             }
         }.execute();
